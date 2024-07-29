94502fba6c6677b8ce3f3d89b4b9ee3e81a5ebc4
dipen.patel <Dipen.patel@radisys.com>
2024-07-29 23:48:29 +0530

    Add Parser initialization

--- server/SIPParser.cpp
+++ server/SIPParser.cpp
@@ -3,9 +3,15 @@
 std::string get_method_from_sip_message(const std::string& raw_message) {
     osip_message_t *sip_message = nullptr;
     int result = osip_message_init(&sip_message);
-    if (result != 0) {
+    if (result != 0 || sip_message == nullptr) {
         std::cerr << "Failed to initialize SIP message" << std::endl;
         return "";
+    }
+
+    if ((parser_init()) < 0) {
+	    fprintf(stderr, "Cannot initialise osip parser.\n");
+	    osip_message_free(sip_message);
+	    return "";
     }
 
     result = osip_message_parse(sip_message, raw_message.c_str(), raw_message.size());
